ccd9e7e77aa5d58e0287e367393440a662f9497d
Bryan Head <bryan.head@gmail.com>
2014-11-25 15:17:21 -0600

    Add parse-row with tests

--- src/main/CSVExtension.scala
+++ src/main/CSVExtension.scala
@@ -1,9 +1,26 @@
 package org.nlogo.extensions.csv
 
+import scala.collection.JavaConverters._
+
 import org.nlogo.api._
+import org.nlogo.api.Syntax._
+
+import org.apache.commons.csv._
+import java.io.StringReader
 
 class CSVExtension extends DefaultClassManager {
+  val format = CSVFormat.DEFAULT
+
+  object ParseRow extends DefaultReporter {
+    override def getSyntax = reporterSyntax(Array(StringType), ListType)
+    override def report(args: Array[Argument], context: Context) = {
+      val row = args(0).getString
+      val record = format.parse(new StringReader(row)).iterator.next
+      LogoList.fromIterator(record.iterator.asScala)
+    }
+  }
   override def load(primManager: PrimitiveManager) = {
     val add = primManager.addPrimitive _
+    add("parse-row", ParseRow)
   }
 }
